bf3ac2b9dad3665a43ed7146b173083077bb4187
dbohdan <dbohdan@dbohdan.com>
2019-03-28 15:35:52 +0200

    fix(main/escapeHtml): escape double quote

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ const escapeHtml = (text) => {
             .replace(/&/g, '&amp;')
             .replace(/</g, '&lt;')
             .replace(/>/g, '&gt;')
-            .replace(/'/g, '&quot;')
+            .replace(/"/g, '&quot;')
             .replace(/'/g, '&#039;');
 };
 
